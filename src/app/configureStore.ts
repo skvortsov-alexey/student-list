c14e3acacc61ca0246e99f65aa051be6a03fd593
skvortsov-alexey <skv.an.89@gmail.com>
2019-12-21 23:58:00 +0600

    Add redux devttool

--- src/app/configureStore.ts
+++ src/app/configureStore.ts
@@ -2,8 +2,21 @@ import { createStore } from 'redux'
 
 import rootReducer from './reducer'
 
-function configureStore() {  
-  return createStore(rootReducer)
+function configureStore() {
+  const store = createStore(
+    rootReducer,
+    (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
+      (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+  )
+  
+  if (process.env.NODE_ENV === 'development' && module.hot) {
+    module.hot.accept('./reducer', () => {
+      const newRootReducer = require('./reducer').default
+      store.replaceReducer(newRootReducer)
+    })
+  }
+
+  return store
 }
 
 export default configureStore
