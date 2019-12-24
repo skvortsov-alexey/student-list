ad1528096431b008f9b6792fd03c176f5f8f9522
skvortsov-alexey <skv.an.89@gmail.com>
2019-12-25 00:41:42 +0600

    Add uid generation for mock data

--- src/api/utils.ts
+++ src/api/utils.ts
@@ -1,27 +1,29 @@
+import v4 from 'uuid/v4'
+
 import { Student, AcademicPerformance } from 'features/students/types'
 import { StudentsStorage } from './types'
 
 const mockStudents: Student[] = [
   {
-    id: '1',
+    id: v4(),
     fullName: 'James Smith',
     birthDate: new Date(2002, 1, 2),
     academicPerformance: AcademicPerformance.Satisfactory
   },
   {
-    id: '2',
+    id: v4(),
     fullName: 'Maria Garcia',
     birthDate: new Date(2001, 4, 3),
     academicPerformance: AcademicPerformance.Good
   },
   {
-    id: '3',
+    id: v4(),
     fullName: 'Osmond Hawarde',
     birthDate: new Date(2002, 6, 2),
     academicPerformance: AcademicPerformance.Excellent
   },
   {
-    id: '4',
+    id: v4(),
     fullName: 'Robert Johnson',
     birthDate: new Date(2000, 9, 11),
     academicPerformance: AcademicPerformance.Unsatisfactory
