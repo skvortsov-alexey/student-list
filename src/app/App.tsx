6901b4c47dc0a62623f24eb30d88cbd43e436d99
skvortsov-alexey <skv.an.89@gmail.com>
2019-12-25 00:45:23 +0600

    Move StudentListView to components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import CssBaseline from '@material-ui/core/CssBaseline'
 
-import StudentListView from 'features/ui/StudentListView'
+import StudentListView from 'components/StudentListView'
 import AppBar from './AppBar'
 
 const App = () => (
